17f464dca30bf2198c2bde59659910cb4896bef9
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-10 12:16:31 -0400

    commiting one

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class SecondOne {
 
 	public static void main(String[] args) {
-		System.out.println("checked in by master");
+		System.out.println("222222222-1");
 
 	}
 
