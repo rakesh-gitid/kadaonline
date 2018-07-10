ff1637c70204f1f58a08ae33ec96d58878f5a342
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-10 11:46:19 -0400

    from one more

--- sampleProject/src/com/example/test/FirstOne.java
+++ sampleProject/src/com/example/test/FirstOne.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class FirstOne {
 
 	public static void main(String[] args) {
-		System.out.println("changed by feature branch one more change");
+		System.out.println("changed by feature branch Lets change this here");
 
 	}
 
