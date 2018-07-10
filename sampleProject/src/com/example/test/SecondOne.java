17669a124c32cb93f4432fe08a512b5bff9c0d2c
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-10 11:25:31 -0400

    commiting

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class SecondOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("checked in by master");
 
 	}
 
