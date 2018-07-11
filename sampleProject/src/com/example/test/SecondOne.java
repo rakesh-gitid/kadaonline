2049447b08f78114bf37caed088fb439edffeb3c
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-11 01:03:05 -0400

    commiting

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class SecondOne {
 
 	public static void main(String[] args) {
-		System.out.println("checked in by new one.....");
+		System.out.println("checked in by new one....second update.");
 
 	}
 
