f49249a470dcff73c42815f145efd17d4951f991
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-11 01:06:22 -0400

    updated

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -3,7 +3,9 @@ package com.example.test;
 public class SecondOne {
 
 	public static void main(String[] args) {
-		System.out.println("checked in by master changing again from one more");
+
+		System.out.println("checked in by new one....second update.");
+
 
 	}
 
