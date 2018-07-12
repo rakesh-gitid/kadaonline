91fbce109c9a0d29700cf8bf89ec4a23b0be16b9
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-12 01:16:06 -0400

    commit

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -4,7 +4,7 @@ public class SecondOne {
 
 	public static void main(String[] args) {
 
-		System.out.println("checked in by master changing again from one more");
+		System.out.println("Trying commit from one more should result in conflict");
 
 
 	}
