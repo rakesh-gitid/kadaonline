e1f2d75a38ab14777e12cc9f5d106d5d7c268919
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-12 01:14:29 -0400

    commit

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class SecondOne {
 
 	public static void main(String[] args) {
-		System.out.println("Tring revert checking in from new one");
+		System.out.println("Trying revert checking in from new one");
 
 	}
 
