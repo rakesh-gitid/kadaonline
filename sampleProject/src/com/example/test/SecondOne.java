cd55d099a05172e31ffdbc09ead81eecf361dd65
Rakesh Raj <rrajj@mcfadyen.com>
2018-07-12 01:12:26 -0400

    commit

--- sampleProject/src/com/example/test/SecondOne.java
+++ sampleProject/src/com/example/test/SecondOne.java
@@ -3,7 +3,7 @@ package com.example.test;
 public class SecondOne {
 
 	public static void main(String[] args) {
-		System.out.println("checked in by new one....second update.");
+		System.out.println("Tring revert checking in from new one");
 
 	}
 
